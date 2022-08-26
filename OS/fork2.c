f46ed56e878efc42fb1ec4d8d00138c66f968ca7
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2022-08-27 01:55:21 +0600

    Add files via upload

---
+++ OS/fork2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(){
+    printf("without fork PID %d\n",getpid());
+    fork();
+    fork();
+    printf("with fork PID %d\n",getpid());
+}
